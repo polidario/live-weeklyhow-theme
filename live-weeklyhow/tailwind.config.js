5bbb723e6791cbfdf357b78f4c70de9cb9b8cddb
Bernard Polidario <bernard@weeklyhow.com>
2023-12-16 12:23:29 +0100

    Added assets/icons for the header.liquid + created header.liquid

--- live-weeklyhow/tailwind.config.js
+++ live-weeklyhow/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./assets/*.*",
+    "./layout/*.liquid",
+    "./sections/*.liquid",
+    "./snippets/*.liquid",
+    "./templates/*.liquid",
+    "./templates/customers/*.liquid"
+  ],
   theme: {
     extend: {},
   },
